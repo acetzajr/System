7532814ccd3fcd6e6539cf5c8068192ac75ecfc5
José Martínez <marquevjosman@gmail.com>
2024-10-31 18:45:41 -0600

    up

--- src/system/main.cpp
+++ src/system/main.cpp
@@ -1,6 +1,56 @@
 #include <fmt/core.h>
+#include <raylib.h>
+
+namespace sys
+{
+
+  class Manager
+  {
+  
+    public:
+      Manager();
+      ~Manager();
+    
+    public:
+      void run();
+      
+  };
+  
+  Manager::Manager()
+  {
+    fmt::println("***Welcome to system!***");
+    SetTargetFPS(30);
+    InitWindow(720, 360, "system");
+    if(!IsWindowFullscreen())
+    {
+      ToggleFullscreen();
+    }
+  }
+  
+  Manager::~Manager()
+  {
+    CloseWindow();
+    fmt::println("***Goodbye from system!***");
+  }
+  
+  void Manager::run()
+  {
+    Color background{211, 176, 131, 255};
+    while(!WindowShouldClose())
+    {
+      BeginDrawing();
+      ClearBackground(background);
+      DrawFPS(12, 12);
+      EndDrawing();
+    }
+  }
+  
+}
+
+using namespace sys;
 
 int main()
 {
-  fmt::println("system");
+  Manager manager;
+  manager.run();
 }
